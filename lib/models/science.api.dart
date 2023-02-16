45074de6622b52dbba5a6d1d9131b33992020bed
Lakshan Kodithuwakku <63406685+LakshanKodithuwakku@users.noreply.github.com>
2023-02-17 00:39:18 +0530

    Add build automation on github

--- lib/models/science.api.dart
+++ lib/models/science.api.dart
@@ -6,7 +6,7 @@ class ScienceNewsApi {
   static Future<List<NewsModel>> getScienceNews(String newsType) async {
     var dio = Dio();
     final response = await dio.get(
-      "https://inshorts.deta.dev/news?category="+ newsType,
+      "https://inshorts.deta.dev/news?category=" + newsType,
     );
     Map<String, dynamic> responseJson = json.decode(response.toString());
     List _temp = responseJson["data"];
